e1a6c35df0c3bf6bb27f0feb90ba850c7f9c462d
Joshua Benjamin <joshuabnjmn@gmail.com>
2015-05-01 11:37:02 -0700

    Fix commit. Still some issues with run

---
+++ README.md
@@ -0,0 +1,8 @@
+Installation
+------------
+1. Install the latest version of [Vagrant](http://www.vagrantup.com/downloads.html)
+2. Install the latest version of [ChefDK](https://downloads.chef.io/chef-dk/)
+3. Install the Vagrant Berkshelf plugin:
+
+        $ vagrant plugin install vagrant-omnibus
+        $ vagrant plugin install vagrant-berkshelf
---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name             'akka-stats'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures akka-stats'
+long_description 'Installs/Configures akka-stats'
+
+version          '0.1.0'
+
+
+depends 'graphite'
+depends 'statsd'
+depends 'grafana'
+depends 'apt'
